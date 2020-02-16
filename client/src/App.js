f19c74d08248443815a0ad196f554da12fddcd6d
Ben Yu <intenex@gmail.com>
2020-02-15 16:01:00 -0800

    Test app

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,36 @@ import "./App.css";
 
 class App extends React.Component {
   // Initialize state
-  state = { passwords: [] };
+  state = { articles: [] };
 
   // Fetch passwords after first mount
   componentDidMount() {
-    this.getPasswords();
+    this.getArticles();
   }
 
-  getPasswords = () => {
-    // Get the passwords and store them in state
-    fetch("/api/passwords")
+  // getPasswords = () => {
+  //   // Get the passwords and store them in state
+  //   fetch("/api/passwords")
+  //     .then(res => res.json())
+  //     .then(passwords => this.setState({ passwords }));
+  // };
+
+  getArticles = () => {
+    // get articles and store them in state
+    fetch("/api/articles")
       .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+      .then(articles => this.setState({ articles }));
   };
 
   render() {
-    const { passwords } = this.state;
+    const { articles } = this.state;
 
     return (
       <div className="App">
         {/* Render the passwords if we have them */}
-        {passwords.length ? (
+        {articles.length ? (
           <div>
-            <h1>5 Passwords.</h1>
+            <h1>Articles</h1>
             <ul className="passwords">
               {/*
                 Generally it's bad to use "index" as a key.
@@ -33,21 +40,15 @@ class App extends React.Component {
                 be the same number of passwords, and they never
                 change positions in the array.
               */}
-              {passwords.map((password, index) => (
-                <li key={index}>{password}</li>
+              {articles.map((article, index) => (
+                <li key={index}>{article}</li>
               ))}
             </ul>
-            <button className="more" onClick={this.getPasswords}>
-              Get More
-            </button>
           </div>
         ) : (
           // Render a helpful message otherwise
           <div>
-            <h1>No passwords :(</h1>
-            <button className="more" onClick={this.getPasswords}>
-              Try Again?
-            </button>
+            <h1>No articles :(</h1>
           </div>
         )}
       </div>
