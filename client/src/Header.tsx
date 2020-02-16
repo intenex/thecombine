bc75e70c63afcbf3edffe314036af6e18ee6f65b
Ben Yu <intenex@gmail.com>
2020-02-15 22:58:32 -0800

    Added deleting and work in progress for editing

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -2,15 +2,61 @@ import React from "react";
 import "./App.css";
 import { Link, withRouter, RouteComponentProps } from "react-router-dom";
 
-interface Props extends RouteComponentProps {
+interface Props extends RouteComponentProps<OptionalArticleParams> {
   onSubmit?: () => unknown;
 }
 
+type OptionalArticleParams = { articleId?: string };
+
 class Header extends React.Component<Props> {
+  handleDelete = async () => {
+    const {
+      match: { params }
+    } = this.props;
+    if (params.articleId) {
+      const response = await fetch(`/api/articles/${params.articleId}`, {
+        method: "DELETE"
+      });
+      const parsedResponse = await response.json();
+      console.log(parsedResponse);
+    }
+  };
+
   render() {
-    const linkTo =
-      this.props.location.pathname === "/editor" ? "/viewer" : "/editor";
-    const editor = this.props.location.pathname === "/editor";
+    const {
+      match: { params }
+    } = this.props;
+    const { pathname } = this.props.location;
+    console.log(this.props.location.pathname);
+    const linkTo = pathname === "/editor" ? "/viewer" : "/editor";
+    const editor = pathname === "/editor";
+    let maybeEditAndDeleteButtons = <></>;
+    if (params.articleId) {
+      if (pathname === `/view/${params.articleId}`) {
+        // then we should show edit and delete buttons
+        const editPage = pathname + "/edit"; // create this route later to edit
+        maybeEditAndDeleteButtons = (
+          <>
+            <Link to={editPage}>
+              {" "}
+              <button
+                type="button"
+                className="btn btn-primary contributeButton"
+              >
+                Edit
+              </button>
+            </Link>
+            <button
+              type="button"
+              className="btn btn-primary contributeButton"
+              onClick={this.handleDelete}
+            >
+              Delete
+            </button>
+          </>
+        );
+      }
+    }
     return (
       <div className="header">
         <div className="row">
@@ -36,6 +82,7 @@ class Header extends React.Component<Props> {
                 </button>
               </Link>
             )}
+            {maybeEditAndDeleteButtons}
           </div>
           <div className="col-2">
             <img
