34fcaa8d0799b4bfedcf1bd8a1326313f09fc5d0
Ben Yu <intenex@gmail.com>
2020-02-15 19:32:02 -0800

    Add update and delete REST endpoints

--- index.js
+++ index.js
@@ -39,6 +39,54 @@ app.post("/api/articles", (req, res) => {
   );
 });
 
+app.get("/api/articles/:id", (req, res) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM articles WHERE id = $1", [id], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
+// better syntax better late than never
+const updateArticle = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { title, body } = request.body;
+  pool.query(
+    "UPDATE articles SET title = $1, body = $2 WHERE id = $3",
+    [title, body, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json({
+        status: "success",
+        message: `Article with ID ${id} successfully updated.`
+      });
+    }
+  );
+};
+
+app.put("/api/articles/:id", updateArticle);
+
+const deleteArticle = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM articles WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json({
+      status: "success",
+      message: `Article with ID ${id} successfully deleted.`
+    });
+  });
+};
+
+app.delete("/api/articles/:id", deleteArticle);
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
